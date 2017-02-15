ea934cb1f863130d46bf9b27bbbb86211f8a08f1
Mashrur Hossain <proror5railsudemy@gmail.com>
2017-02-15 17:06:15 +0000

    Make temp bug fix

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -15,7 +15,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new(chef_params)
     if @chef.save
       session[:chef_id] = @chef.id
-      cookies.signed[:chef_id] = chef.id
+      #cookies.signed[:chef_id] = chef.id
       flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
       redirect_to chef_path(@chef)
     else
